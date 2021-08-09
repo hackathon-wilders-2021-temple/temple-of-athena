534fcf0679b55a545088c9b01ee6b4ca1bebf94b
Lorianne Aguilar <78496780+Grailsidhe@users.noreply.github.com>
2021-08-09 21:57:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <BrowserRouter basename="https://hackathon-wilders-2021-temple.github.io/temple-of-athena">
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
